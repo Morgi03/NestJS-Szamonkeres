0266617db9f28ffd3add6dbff1d58428eb5f6e73
Morgován Nimród <morgovannimrod22@gmail.com>
2022-12-14 13:24:21 +0100

    kiiratás, de még kell módósítani a Like-on

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CatDto } from './Cat.dto';
 import db from './db';
 
 @Controller()
@@ -8,7 +9,22 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index() {
-    return {  };
+  async listcats(
+    @Query('szem_szin')
+    szem_szin = 'zöld',
+  ) {
+    const [rows] = await db.execute(
+      'SELECT id, suly, szem_szin FROM macskak WHERE szem_szin LIKE(?) ORDER BY suly DESC',
+      [szem_szin],
+    );
+    return {
+      cats: rows,
+    };
+  }
+
+  @Get('cats/new')
+  @Render('form')
+  newPaintingForm() {
+    return {};
   }
 }
